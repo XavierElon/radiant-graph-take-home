fc34a8b7d1ac3a07d48246563fae3bc50a51dbbf
Xavier <xavierh93@hotmail.com>
2025-06-11 08:04:01 -0700

    update imports

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,6 @@
-from .customers import router as customers_router
-from .health import router as health_router
-from .orders import router as orders_router
-from .analytics import router as analytics_router 
+from .routes.customers import router as customers_router
+from .routes.orders import router as orders_router
+from .routes.analytics import router as analytics_router
+from .routes.health import router as health_router
+
+__all__ = ['customers_router', 'orders_router', 'analytics_router', 'health_router'] 
